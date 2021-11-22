1dd1ce4c0a417675b7955c1bc6679cd826814d85
islamay <islamay.dean@gmail.com>
2021-11-22 19:44:08 +0800

    Closed #1

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,16 +1,30 @@
-import { FC, useState } from 'react'
-import Navbar from 'src/components/Navbar'
+import { FC, useState, CSSProperties } from 'react'
+import NewsCard from 'src/components/NewsCard'
 
 const Test: FC = () => {
-    const [openFilter, setOpenFilter] = useState(false)
 
     return (
-        <div>
-            <Navbar
-                onSearchTermChange={() => { }}
+        <div style={style}>
+            <NewsCard
+                src='https://tabloidnyata.com/wp-content/uploads/2017/06/mr-robot-season-2-after-show.jpg'
+                category='Other'
+                title='Mr Robot Season 5 is Out!'
+                publisher='Dean'
+                date='12/1/21'
+                time='9:30'
+                to='/'
             />
         </div>
     )
+}
+
+const style: CSSProperties = {
+    padding: 20,
+    backgroundColor: 'var(--dark)',
+    minHeight: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
 }
 
 export default Test
--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,10 +1,54 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/dist/client/router'
+import styles from 'src/styles/NewsCard.module.css'
 
-const NewsCard = () => {
+interface props {
+    src: string,
+    category: String,
+    title: string,
+    publisher: string,
+    date: string,
+    time: string,
+    to: string
+}
+
+const NewsCard: FC<props> = ({
+    src,
+    category,
+    title,
+    publisher,
+    date,
+    time,
+    to
+}) => {
+    const router = useRouter()
+
+    useEffect(() => {
+        router.prefetch(to)
+        router.prefetch(`/category=${category}`)
+    }, [])
+
+    const handleCategoryClick = () => {
+        router.push(`/?category=${category}`)
+    }
+
+    const handleTitleClick = () => {
+        router.push(to)
+    }
+
 
     return (
-        <div>
-            <h1>NewsCard</h1>
+        <div className={styles.container}>
+            <img
+                className={styles.image}
+                src={src}
+            />
+            <div className={styles['card-body']}>
+                <h4 onClick={handleCategoryClick} className={styles.category}>{category}</h4>
+                <h3 onClick={handleTitleClick} className={styles.title}>{title}</h3>
+                <h5 className={styles.publisher}>Oleh {publisher}&ensp;&ensp;{date}&ensp;&ensp;{time}</h5>
+            </div>
         </div>
     )
 }
