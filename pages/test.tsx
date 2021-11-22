e9f062512d20028e318245cad88e11529a11c1d9
islamay <islamay.dean@gmail.com>
2021-11-22 16:46:58 +0800

    Create: Test.tsx for testing component

---
+++ pages/test.tsx
@@ -0,0 +1,14 @@
+import { FC, useState } from 'react'
+import Navbar from 'src/components/Navbar'
+
+const Test: FC = () => {
+    const [openFilter, setOpenFilter] = useState(false)
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Test
